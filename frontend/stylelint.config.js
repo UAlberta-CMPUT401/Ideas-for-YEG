ca5b2b594723f87b4b131e99ce05569d6ea62f2b
bui1@ualberta.ca <bui1@ualberta.ca>
2020-02-01 15:53:59 -0700

    add vuetify library for reusable ui components:

--- frontend/stylelint.config.js
+++ frontend/stylelint.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
+  "rules": {
+    "block-no-empty": null,
+    "color-no-invalid-hex": true,
+    "comment-empty-line-before": [ "always", {
+      "ignore": ["stylelint-commands", "after-comment"]
+    } ],
+    "declaration-colon-space-after": "always",
+    "max-empty-lines": 2,
+    "rule-empty-line-before": [ "always", {
+      "except": ["first-nested"],
+      "ignore": ["after-comment"]
+    } ],
+  }
 }
