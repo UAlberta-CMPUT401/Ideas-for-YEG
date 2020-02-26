6dbcdbe75add3ad053c5e35f674b998576b14107
ChrisChrisLoLo <sporewoh@gmail.com>
2020-02-26 11:28:32 -0700

    Add basic search functionality, add new field to count upvotes

--- backend/api/idea/controllers/idea.js
+++ backend/api/idea/controllers/idea.js
@@ -25,7 +25,7 @@ module.exports = {
     let entity;
     entity = await strapi.services.idea.findOne(ctx.params);
 
-    var upvoters = entity.user_upvoters;
+    let upvoters = entity.user_upvoters;
     if (upvoters.find(element => element.id == ctx.state.user.id) != undefined) {
       upvoters = removeUserArray(upvoters, ctx.state.user)
     } else {
@@ -47,7 +47,7 @@ module.exports = {
     let entity;
     entity = await strapi.services.idea.findOne(ctx.params);
 
-    var volunteers = entity.volunteers;
+    let volunteers = entity.volunteers;
     if (volunteers.find(element => element.id == ctx.state.user.id) != undefined) {
       volunteers = removeUserArray(volunteers, ctx.state.user)
     } else {
@@ -69,7 +69,7 @@ module.exports = {
     let entity;
     entity = await strapi.services.idea.findOne(ctx.params);
 
-    var followers = entity.followers;
+    let followers = entity.followers;
     if (followers.find(element => element.id == ctx.state.user.id) != undefined) {
       followers = removeUserArray(followers, ctx.state.user)
     } else {
@@ -87,9 +87,50 @@ module.exports = {
   },
 
   async search(ctx){
-    const result = strapi.query('idea').model.find({
+    let resLimit = 100;
+    let skip = 0;
+    const queryParams = {$and:[]};
+    let sortBy = {'createdAt':1};
 
-    });
+    // Query params include: searchTerm, sortBy, limit, skip,
+    const qParams = ctx.request.query;
+    if( qParams.locId ){
+      let loc = await strapi.query('location').findOne({route: qParams.locId});
+      if(!loc){
+        ctx.throw(400, 'invalid location id provided');
+      }
+      queryParams.$and.push({location: loc._id})
+    }
+    else{
+      ctx.throw(400, 'location id parameter (locId) required');
+    }
+
+    if( qParams.searchTerm ){
+      // Push clause that looks if titles or documents contain the search term as a substring
+      queryParams.$and.push({
+        $or:[
+          {"title" : {$regex : `.*${qParams.searchTerm}.*`, $options : 'i'}},
+          {"description" : {$regex : `.*${qParams.searchTerm}.*`, $options : 'i'}},
+          // {"categories" : {$regex : `.*${qParams.searchTerm}.*`}},
+          ]
+      });
+    }
+    if( qParams.sortBy ){
+      if( qParams.sortBy === 'new'){
+        sortBy = {'createdAt':1};
+      }
+      else if (qParams.sortBy === 'top'){
+        sortBy = {};
+      }
+    }
+    if( qParams.limit && qParams.limit < resLimit ){
+      resLimit = qParams.limit;
+    }
+    if( qParams.skip ){
+      skip = qParams.skip;
+    }
+
+    const result = strapi.query('idea').model.find(queryParams).limit(resLimit).skip(skip).sort(sortBy);
 
     return result;
   }
