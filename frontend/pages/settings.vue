849106d505f5aa8582f122f5b67829437182dd90
bui1@ualberta.ca <bui1@ualberta.ca>
2020-03-15 19:54:02 -0600

    Add update user picture api call and snackbar result

--- frontend/pages/settings.vue
+++ frontend/pages/settings.vue
@@ -7,6 +7,14 @@
       <span>{{ snackbarMessage }}</span>
     </v-snackbar>
 
+    <v-overlay :value="loading">
+      <v-progress-circular
+        :size="200"
+        color="primary"
+        indeterminate
+      ></v-progress-circular>
+    </v-overlay>
+
     <v-layout column justify-center align-center>
       <v-flex xs12 sm8 md6>
         <v-card class="mx-auto divider" width="400" outlined>
@@ -14,19 +22,20 @@
             Change Your Avatar
           </v-card-title>
           <v-list-item three-line class="justify-center">
-            <v-list-item-avatar
-              tile
-              size="150"
-              color="grey"
+            <v-list-item-avatar tile size="150" color="grey"
+              ><v-img :src="currentAvatar"></v-img
             ></v-list-item-avatar>
           </v-list-item>
 
           <v-card-actions>
             <v-file-input
-              accept="image/png, image/jpeg, image/bmp"
               placeholder="Pick an avatar"
               prepend-icon="mdi-camera"
               label="Avatar"
+              ref="file"
+              v-model="image"
+              @change.self="changeAvatar"
+              :clearable="false"
             ></v-file-input>
           </v-card-actions>
         </v-card>
@@ -64,8 +73,8 @@
                 name="ConfirmPassword"
                 label="Confirm New Password"
               ></v-text-field>
-              <v-btn v-on:click="changePassword">Change Password</v-btn>
             </v-form>
+            <v-btn v-on:click="changePassword">Change Password</v-btn>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -74,17 +83,23 @@
 </template>
 
 <script>
+import { DEFAULT_AVATAR_IMG_PATH } from '../constants/constants';
+
 const PASSWORD_MIN_LENGTH = 6;
 const PASSWORD_MAX_LENGTH = 32;
+const PICTURE_UPLOAD_ERROR =
+  'Error with uploading new profile avatar. Please try again';
 
 export default {
   consts: [PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH],
 
   data() {
     return {
-      error: false,
-      success: false,
+      snackbarError: false,
+      snackbarSuccess: false,
       snackbarMessage: '',
+      image: '',
+      currentAvatar: DEFAULT_AVATAR_IMG_PATH,
       currentPassword: '',
       newPassword: '',
       passwordConf: '',
@@ -99,12 +114,89 @@ export default {
       passwordConfRules: [
         (v) => v === this.password || 'Passwords do not match',
       ],
+      loading: false,
     };
   },
 
+  async mounted() {
+    const userJSON = window.localStorage.getItem('userData');
+    const userData = JSON.parse(userJSON);
+
+    const config = {
+      headers: {
+        Authorization: 'Bearer ' + userData.jwt,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const userResponse = await this.$axios
+      .$get('/users/me', config)
+      .catch((error) => console.log(error));
+
+    if (userResponse) {
+      this.currentAvatar = userResponse.avatar
+        ? `${this.$axios.defaults.baseURL}${userResponse.avatar.url}`
+        : DEFAULT_AVATAR_IMG_PATH;
+    }
+  },
+
   methods: {
-    changePassword() {
-      this.$refs.form.validate();
+    async changeAvatar(event) {
+      this.loading = true;
+      const formData = new FormData();
+
+      console.log(this.image, this.image.name);
+
+      formData.append(`files`, this.image, this.image.name);
+
+      const userJSON = window.localStorage.getItem('userData');
+      const userData = JSON.parse(userJSON);
+
+      console.log(userData);
+
+      const config = {
+        headers: {
+          Authorization: 'Bearer ' + userData.jwt,
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const imageResponse = await this.$axios
+        .$post('/upload', formData, {
+          headers: {
+            Authorization: 'Bearer ' + userData.jwt,
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      if (!imageResponse) {
+        this.snackbarMessage = PICTURE_UPLOAD_ERROR;
+        this.snackbarError = true;
+        this.loading = false;
+        return;
+      }
+
+      const request = {
+        avatar: imageResponse[0].id,
+      };
+
+      const updateUserResponse = await this.$axios
+        .$put(`/users/${userData.user.id}`, request, config)
+        .catch((error) => console.log(error));
+
+      if (updateUserResponse) {
+        this.currentAvatar = `${this.$axios.defaults.baseURL}${updateUserResponse.avatar.url}`;
+        this.loading = false;
+        this.snackbarMessage = 'Profile avatar successfully changed';
+        this.snackbarSuccess = true;
+      } else {
+        this.loading = false;
+        this.snackbarError = true;
+        this.snackbarMessage = PICTURE_UPLOAD_ERROR;
+      }
     },
   },
 };
