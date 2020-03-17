b1ca6875d1711b86eb00d4f9e50e4d9c1fe1338d
relhajj <relhajj@users.noreply.github.com>
2020-03-17 17:40:01 -0600

    made error page look more consistent with rest of app

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,14 +1,33 @@
 <template>
   <v-app>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container fill-height>
+      <v-layout row wrap align-center>
+        <v-flex class="text-xs-center">
+          <v-card class="mx-auto" max-width="350" elevation="12">
+            <v-card-title color="primary">
+              <h1 v-if="error.statusCode === 404">
+                {{ pageNotFound }}
+              </h1>
+              <h1 v-else>
+                {{ otherError }}
+              </h1>
+            </v-card-title>
+
+            <v-card-text>
+              Look's like something went wrong. No worries! Just click the
+              button below to go back to the home page.
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <NuxtLink to="/">
+                <v-btn class="ma-2" color="primary">Home Page</v-btn>
+              </NuxtLink>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-app>
 </template>
 
