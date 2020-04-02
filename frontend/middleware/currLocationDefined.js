d8f51b870ec80456362c5e7b411f22997c953611
ChrisChrisLoLo <sporewoh@gmail.com>
2020-04-02 14:06:07 -0600

    Add redirect if the index page is loaded at the start of a new session

--- frontend/middleware/currLocationDefined.js
+++ frontend/middleware/currLocationDefined.js
@@ -1,8 +1,6 @@
 export default function({ store, redirect }) {
   // If the user has a current location picked
   if (store.state.currLocation.currLocation) {
-    return redirect(
-      `/${store.state.currLocation.currLocation.code}/idea-dashboard`,
-    );
+    return redirect(`/${store.state.currLocation.currLocation}/idea-dashboard`);
   }
 }
